868b1cf5e5fb12c1b87d17b00a26414af759519f
Victor Aguboshim <victor.aguboshim@tum.de>
2017-06-06 22:46:12 +0200

    adding router

--- my-learning-app/src/app/app.module.ts
+++ my-learning-app/src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { ContactUSComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUSComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'contact-us',
+        component: ContactUSComponent
+      } ,
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
